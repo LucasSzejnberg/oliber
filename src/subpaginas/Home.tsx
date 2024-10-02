3e48019cc4133e806dfd1ff45b20bac364559bbd
Lucasszejn <127147761+LucasSzejnberg@users.noreply.github.com>
2024-10-01 22:23:07 -0300

    mal ahi

--- src/subpaginas/Home.tsx
+++ src/subpaginas/Home.tsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
-
+import ContenidoHome from "./components/ContenidoHome";
 const Home = () => {
     return (
         
       <div>
         <Header></Header>
+        <ContenidoHome></ContenidoHome>
       </div>
     );
   };
