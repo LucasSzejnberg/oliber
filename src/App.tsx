87ed49c8fe009c800210ced528fb48f25f36735b
Lucasszejn <127147761+LucasSzejnberg@users.noreply.github.com>
2024-10-01 21:10:29 -0300

    router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./subpaginas/Home.tsx";
+import InicioSesion from "./subpaginas/InicioSesion.tsx";
 
+function App() {
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/2" element={<InicioSesion />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App; // <-- Asegúrate de exportar el componente
